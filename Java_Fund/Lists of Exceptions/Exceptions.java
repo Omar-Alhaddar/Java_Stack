5ef233e0f8307fdc0627b383d7fdc0b9120544f1
Omar Alhaddar <omaralhaddar11@gmail.com>
2021-06-11 21:21:01 +0300

    list of exception assignment

---
+++ Java_Fund/Lists of Exceptions/Exceptions.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Exceptions{
+
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        try {
+
+            for(int i = 0; i < myList.size(); i++) {
+                Integer castedValue = (Integer) myList.get(i);
+            }
+  
+        } catch (Exception e) {
+            System.out.println("Error: 404 hahaha ");
+        }
+
+        
+    }
+}
