7fdaa8324873bf51951beeac713929ed3beda99a
Omar Alhaddar <omaralhaddar11@gmail.com>
2021-06-20 18:01:39 +0300

    Update And Delete Assignment

---
+++ Java_Spring/UpdateDelete/src/main/java/com/example/UpdateDelete/UpdateDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.UpdateDelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateDeleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateDeleteApplication.class, args);
+	}
+
+}
---
+++ Java_Spring/UpdateDelete/src/test/java/com/example/UpdateDelete/UpdateDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UpdateDelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
