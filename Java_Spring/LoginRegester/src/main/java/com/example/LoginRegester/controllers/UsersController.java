5f8f5cb0e9241f3453330ca76f22e4e7a820e293
Omar Alhaddar <omaralhaddar11@gmail.com>
2021-06-27 14:02:43 +0300

    Login and Register full...

---
+++ Java_Spring/LoginRegester/src/main/java/com/example/LoginRegester/controllers/UsersController.java
@@ -0,0 +1,88 @@
+package com.example.LoginRegester.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.LoginRegester.models.User;
+import com.example.LoginRegester.services.UserService;
+import com.example.LoginRegester.validation.UserValidator;
+
+@Controller
+public class UsersController {
+private final UserService userService;
+private final UserValidator userValidator;
+    
+    public UsersController(UserService userService , UserValidator userValidator) {
+        this.userService = userService;
+        this.userValidator = userValidator;
+    }
+    
+    
+    @RequestMapping("/")
+    public String root() {
+    return "redirect:/registration";
+    }
+
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+    	userValidator.validate(user, result);
+    	if (result.hasErrors()) {
+    		return "registrationPage.jsp";
+    	}
+    	else {
+    		userService.registerUser(user);
+    		session.setAttribute("userId", user.getId());
+    		return "redirect:/home";
+    	}
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+        // if the user is authenticated, save their user id in session
+        // else, add error messages and return the login page
+    	
+    	if (userService.authenticateUser(email, password)) {
+    		session.setAttribute("userId", userService.findByEmail(email).getId());
+    		return "redirect:/home";
+    	}
+    	else {
+    		model.addAttribute("error", "invalid user name or password");
+    		return "loginPage.jsp";
+    	}
+    	
+    	
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+        // get user from session, save them in the model and return the home page
+    	if (session.getAttribute("userId")== null)
+    		return "redirect:/registration";
+    	
+    	model.addAttribute("userId", session.getAttribute("userId"));
+    	return "homePage.jsp";
+    }
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+    	session.invalidate();
+    	return "redirect:/login";
+    }
+}
---
+++ Java_Spring/LoginRegester/src/test/java/com/example/LoginRegester/LoginRegesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginRegester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
